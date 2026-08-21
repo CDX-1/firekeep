8346773920bebd0aec7a5de97225ab70fe9d731a
CDX <80155611+CDX-1@users.noreply.github.com>
2026-08-21 19:45:14 -0400

    initiated fabric mod

---
+++ fabric/src/client/java/dev/awsaf/firekeep/client/FirekeepClient.java
@@ -0,0 +1,10 @@
+package dev.awsaf.firekeep.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirekeepClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ fabric/src/main/java/dev/awsaf/firekeep/Firekeep.java
@@ -0,0 +1,10 @@
+package dev.awsaf.firekeep;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Firekeep implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
